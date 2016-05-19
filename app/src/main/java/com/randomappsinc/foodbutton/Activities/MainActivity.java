961b9471a83614fa7726d7afac713f5052204590
Gear61 <chessnone@yahoo.com>
2016-05-18 22:42:39 -0700

    Fix Samsung restart and restaurant info width bug

--- app/src/main/java/com/randomappsinc/foodbutton/Activities/MainActivity.java
+++ app/src/main/java/com/randomappsinc/foodbutton/Activities/MainActivity.java
@@ -33,6 +33,14 @@ public class MainActivity extends AppCompatActivity implements NavigationDrawerF
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Kill activity if it's not on top of the stack due to Samsung bug
+        if (!isTaskRoot() && getIntent().hasCategory(Intent.CATEGORY_LAUNCHER)
+                && getIntent().getAction() != null && getIntent().getAction().equals(Intent.ACTION_MAIN)) {
+            finish();
+            return;
+        }
+
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
